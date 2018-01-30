be2ca32ecbcf03d7285cd4711ec911d151acbe45
Julia Reid <jdereid@gmail.com>
2018-01-29 17:58:37 -0800

    stuff

---
+++ src/main/java/autonomous/AutoPathValueNode.java
@@ -0,0 +1,45 @@
+package autonomous;
+
+import org.team5940.pantry.logging.loggers.Logger;
+import org.team5940.pantry.processing_network.Network;
+import org.team5940.pantry.processing_network.ValueNode;
+
+import autonomous.AutoPathValueNode.Paths;
+
+public class AutoPathValueNode extends ValueNode<Paths> {
+
+	public enum Paths {
+		AUTOLINE(AutoStuff.MOVE_FORWARD , AutoStuff.DELIVER_CUBE), CENTERLEFTSWITCH, CENTERRIGHTSWITCH, LEFTSWITCH, RIGHTSWITCH, RIGHTSCALE, LEFTSCALE;
+		
+		AutoStuff[] plan;
+		
+		Paths(AutoStuff... plan) {
+			this.plan = plan;
+		}
+		
+		public AutoStuff[] getPlan() {
+			return plan;
+		}
+		
+		
+	}
+	
+	public enum AutoStuff {
+		MOVE_FORWARD, TURN_LEFT, DELIVER_CUBE;
+	}
+
+	public AutoPathValueNode(Network network, Logger logger, ValueNode<Paths>[] sourcesArray)
+			throws IllegalArgumentException, IllegalStateException {
+		super(network, logger, sourcesArray);
+		
+		
+	}
+
+	@Override
+	protected Paths updateValue() {
+		
+		return Paths.AUTOLINE; 
+
+	}
+
+}
