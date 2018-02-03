b4c90105091d12d6323aa0623581a638732dcd6f
BertTurtle <mbentley124@gmail.com>
2018-02-02 19:59:08 -0800

    Added an OuttakeCubeAutoAction

---
+++ src/main/java/frc/team5940/codebase2018/robot/autonomous/OuttakeCubeAutoAction.java
@@ -0,0 +1,33 @@
+package frc.team5940.codebase2018.robot.autonomous;
+
+/**
+ * This action causes the robot to outtake the cube, after the elevator reaches
+ * the specified height.
+ * 
+ * @author Michael Bentley
+ *
+ */
+public class OuttakeCubeAutoAction implements ElevatorDependentAutoAction, AutoAction {
+
+	/**
+	 * The required height of the elevator.
+	 */
+	ElevatorHeight requiredHeight;
+
+	/**
+	 * This action causes the robot to outtake the cube, after the elevator reaches
+	 * the set height.
+	 * 
+	 * @param requiredHeight
+	 *            The required elevator height before outtaking the cube.
+	 */
+	public OuttakeCubeAutoAction(ElevatorHeight requiredHeight) {
+		this.requiredHeight = requiredHeight;
+	}
+
+	@Override
+	public ElevatorHeight getRequiredElevatorHeight() {
+		return this.requiredHeight;
+	}
+
+}
