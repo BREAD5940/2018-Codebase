76b76b800b6a78e44ffff89248d21e8bdab6b7d0
BertTurtle <mbentley124@gmail.com>
2018-02-02 19:52:06 -0800

    Added most teleop functionality to robot. Will add ramps shortly.

--- src/main/java/frc/team5940/codebase2018/robot/Robot.java
+++ src/main/java/frc/team5940/codebase2018/robot/Robot.java
@@ -5,31 +5,23 @@ import org.team5940.pantry.logging.loggers.PrintStreamLogger;
 import org.team5940.pantry.processing_network.Network;
 import org.team5940.pantry.processing_network.ValueNode;
 import org.team5940.pantry.processing_network.ctre.output.TalonSRXNode;
-import org.team5940.pantry.processing_network.data_flow.MultiplexerValueNodeTest.TestEnum;
-import org.team5940.pantry.processing_network.functional.ChangeDetectorNode;
 import org.team5940.pantry.processing_network.functional.ConstantValueNode;
-import org.team5940.pantry.processing_network.functional.DeadzoneValueNode;
-import org.team5940.pantry.processing_network.functional.MaxValueValueNode;
 import org.team5940.pantry.processing_network.functional.MultiplexerValueNode;
 import org.team5940.pantry.processing_network.wpilib.input.HIDAxisValueNode;
-import org.team5940.pantry.processing_network.wpilib.input.HIDButtonValueNode;
 import org.team5940.pantry.processing_network.wpilib.input.RobotStateValueNode;
 import org.team5940.pantry.processing_network.wpilib.input.RobotStateValueNode.RobotState;
 import org.team5940.pantry.processing_network.wpilib.output.DoubleSolenoidNode;
 import org.team5940.pantry.processing_network.wpilib.systems.MaxSpeedValueNode;
 import org.team5940.pantry.processing_network.wpilib.systems.ShiftingNodeGroup;
-import org.team5940.pantry.processing_network.wpilib.systems.SolenoidTwoBooleanControllerValueNode;
 import org.team5940.pantry.processing_network.wpilib.systems.VelocityControlNodeGroup;
 import org.team5940.pantry.processing_network.wpilib.systems.controller_layouts.XBoxControllerLayout;
+import org.team5940.pantry.processing_network.wpilib.systems.encoder_conversion.MeasurementToEncoderValueNode;
 
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
-import frc.team5940.codebase2018.robot.autonomous.AutoPlanFollower;
-import frc.team5940.codebase2018.robot.autonomous.AutonomousAction;
-import frc.team5940.codebase2018.robot.autonomous.MoveForwardAction;
 import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.Joystick;
 
@@ -46,22 +38,12 @@ public class Robot extends IterativeRobot {
 	TalonSRX slaveRight = new TalonSRX(3);
 	TalonSRX masterRight = new TalonSRX(4);
 
-	DoubleSolenoid shiftingSolenoid;
+	Joystick primaryJoystick = new Joystick(0);
+	Joystick secondaryJoystick = new Joystick(1);
 
-	Joystick joystick = new Joystick(0);
 	Network network;
 
 	// Logger logger = new PrintStreamLogger(System.out);
-
-	private float ciabattaLowGearPositionP = 0.5f;
-	private float ciabattaLowGearPositionI = 0f;
-	private float ciabattaLowGearPositionD = 20f;
-	private float ciabattaLowGearPositionF = 0f;
-
-	private float ciabattaLowGearVelocityP = 0.9f;
-	private float ciabattaLowGearVelocityI = 0.0025f;
-	private float ciabattaLowGearVelocityD = 40f;
-	private float ciabattaLowGearVelocityF = 0.15f;
 
 	// private float lP = 0.5f;
 	// private float lI = 0.5f;
@@ -73,9 +55,6 @@ public class Robot extends IterativeRobot {
 	float maxVelocity = ciabattaHighGearMaxVelocity;
 
 	private final float wheelDiameter = 3.5f / 12;
-
-	private final float velocityPulsesPerRotation = 409.6f;
-	private final float distancePulsesPerRotation = 4096f;
 
 	@Override
 	public void disabledInit() {
@@ -94,70 +73,166 @@ public class Robot extends IterativeRobot {
 			e.printStackTrace();
 		}
 
-		this.shiftingSolenoid = new DoubleSolenoid(9, 6, 7);
-
 		masterRight.setSelectedSensorPosition(0, 0, 0);
+		masterLeft.setSelectedSensorPosition(0, 0, 0);
 		network = new Network(10000, logger);
 
-		// SHIFTING
-		ShiftingNodeGroup shifting = new ShiftingNodeGroup(network, logger, "Shifting Node Group", joystick,
-				XBoxControllerLayout.Buttons.RB_BUTTON, XBoxControllerLayout.Buttons.LB_BUTTON, shiftingSolenoid,
-				Value.kReverse);
+		// SHIFTING SETUP
+		DoubleSolenoid drivetrainShiftingSolenoid = new DoubleSolenoid(RobotConfig.SOLENOID_MODULE_NUMBER,
+				RobotConfig.DRIVETRAIN_SOLENOID_SHIFTING_FORWARD_CHANNEL,
+				RobotConfig.DRIVETRAIN_SOLENOID_SHIFTING_REVERSE_CHANNEL);
 
-		MaxSpeedValueNode maxSpeed = new MaxSpeedValueNode(network, logger, shifting.getSolenoidController(),
-				Value.kForward, ciabattaLowGearMaxVelocity, ciabattaHighGearMaxVelocity);
+		ShiftingNodeGroup drivetrainShiftingNodeGroup = new ShiftingNodeGroup(network, logger, "Shifting Node Group",
+				primaryJoystick, XBoxControllerLayout.Buttons.RB_BUTTON, XBoxControllerLayout.Buttons.LB_BUTTON,
+				drivetrainShiftingSolenoid, Value.kReverse);
 
-		// TALON SETUP
+		new DoubleSolenoidNode(network, logger, "Drivetrain Shifting", true,
+				drivetrainShiftingNodeGroup.getSolenoidController(), drivetrainShiftingSolenoid);
+
+		// DRIVETRAIN TALON SETUP
 		slaveRight.set(ControlMode.Follower, masterRight.getDeviceID());
 		slaveLeft.set(ControlMode.Follower, masterLeft.getDeviceID());
 
 		masterLeft.setInverted(true);
 		slaveLeft.setInverted(true);
 
-		masterRight.config_kP(0, ciabattaLowGearVelocityP, 0);
-		masterRight.config_kI(0, ciabattaLowGearVelocityI, 0);
-		masterRight.config_kD(0, ciabattaLowGearVelocityD, 0);
-		masterRight.config_kF(0, ciabattaLowGearVelocityF, 0);
+		masterRight.config_kP(0, RobotConfig.CIABATTA_LOW_GEAR_VELOCITY_P, 0);
+		masterRight.config_kI(0, RobotConfig.CIABATTA_LOW_GEAR_VELOCITY_I, 0);
+		masterRight.config_kD(0, RobotConfig.CIABATTA_LOW_GEAR_VELOCITY_D, 0);
+		masterRight.config_kF(0, RobotConfig.CIABATTA_LOW_GEAR_VELOCITY_F, 0);
 
-		masterLeft.config_kP(0, ciabattaLowGearVelocityP, 0);
-		masterLeft.config_kI(0, ciabattaLowGearVelocityI, 0);
-		masterLeft.config_kD(0, ciabattaLowGearVelocityD, 0);
-		masterLeft.config_kF(0, ciabattaLowGearVelocityF, 0);
+		masterLeft.config_kP(0, RobotConfig.CIABATTA_LOW_GEAR_VELOCITY_P, 0);
+		masterLeft.config_kI(0, RobotConfig.CIABATTA_LOW_GEAR_VELOCITY_I, 0);
+		masterLeft.config_kD(0, RobotConfig.CIABATTA_LOW_GEAR_VELOCITY_D, 0);
+		masterLeft.config_kF(0, RobotConfig.CIABATTA_LOW_GEAR_VELOCITY_F, 0);
 
-		// DRIVETRAIN
-		VelocityControlNodeGroup encoderSpeed = new VelocityControlNodeGroup(network, logger, "Drivetrain", joystick,
-				XBoxControllerLayout.Axis.LEFT_JOYSTICK_Y, XBoxControllerLayout.Axis.RIGHT_JOYSTICK_X, 0.01, 0.01,
-				maxSpeed, wheelDiameter, velocityPulsesPerRotation);
+		// DRIVETRAIN NODE SETUP
+		MaxSpeedValueNode maxSpeed = new MaxSpeedValueNode(network, logger,
+				drivetrainShiftingNodeGroup.getSolenoidController(), Value.kForward, ciabattaLowGearMaxVelocity,
+				ciabattaHighGearMaxVelocity);
 
-		RobotStateValueNode robotState = new RobotStateValueNode(network, logger, "Robot State", this);
+		VelocityControlNodeGroup encoderSpeed = new VelocityControlNodeGroup(network, logger, "Drivetrain",
+				primaryJoystick, XBoxControllerLayout.Axis.LEFT_JOYSTICK_Y, XBoxControllerLayout.Axis.RIGHT_JOYSTICK_X,
+				0.01, 0.01, maxSpeed, wheelDiameter, RobotConfig.VELOCITY_PULSES_PER_ROTATION);
 
-		// TODO Placeholder. 
-		ValueNode<Double> autonLeftDrivetrainValueNode = null;
-		ValueNode<Double> autonRightDrivetrainValueNode = null;
+		ConstantValueNode<ControlMode> controlMode = new ConstantValueNode<ControlMode>(network, logger,
+				"Drivetrain Control Mode", ControlMode.Velocity);
 
-		ValueNode<Double> defaultRobotSpeed = new ConstantValueNode<Double>(network, logger, "Default Robot Speed", 0d);
+		new TalonSRXNode(network, logger, "Right Talon", true, controlMode, encoderSpeed.getRightEncoderValue(),
+				masterRight);
+		new TalonSRXNode(network, logger, "Left Talon", true, controlMode, encoderSpeed.getLeftEncoderValue(),
+				masterLeft);
 
-		MultiplexerValueNode<Double, RobotState> leftRobotSpeed = Robot.<Double>generateAutonMultiplexerValueNode(
-				network, logger, "Left Drivetrain Multiplexer", robotState, autonLeftDrivetrainValueNode,
-				encoderSpeed.getLeftEncoderValue(), defaultRobotSpeed);
+		// ELEVATOR TALON SETUP
+		TalonSRX masterElevatorTalon = new TalonSRX(RobotConfig.MASTER_ELEVATOR_TALON_PORT);
+		TalonSRX slaveElevatorTalon = new TalonSRX(RobotConfig.SLAVE_ELEVATOR_TALON_PORT);
 
-		MultiplexerValueNode<Double, RobotState> rightRobotSpeed = Robot.<Double>generateAutonMultiplexerValueNode(
-				network, logger, "Right Drivetrain Multiplexer", robotState, autonRightDrivetrainValueNode,
-				encoderSpeed.getRightEncoderValue(), defaultRobotSpeed);
-		
-		// TODO Placeholder. 
-		AutonomousAction[] autoPlanActions = {new MoveForwardAction(14, 3), new DeliverCubeAction(3)};
-		
-		ConstantValueNode<AutonomousAction[]> autoPlan = new ConstantValueNode<AutonomousAction[]>(network, logger, "Auto Plan", autoPlanActions);
-		
-		AutoPlanFollower currentActionNode = new AutoPlanFollower(network, logger, "Auto Plan Follower", autoPlan);
-		
-		TalonSRXControlModeValueNode controlMode = new TalonSRXControlModeValueNode(network, logger, "Drive Train Control Mode", robotState, currentActionNode);
-		
-		new TalonSRXNode(network, logger, "Right Talon", true, controlMode, rightRobotSpeed, masterRight);
-		new TalonSRXNode(network, logger, "Left Talon", true, controlMode, leftRobotSpeed, masterLeft);
+		slaveElevatorTalon.set(ControlMode.Follower, RobotConfig.MASTER_ELEVATOR_TALON_PORT);
+
+		masterElevatorTalon.config_kP(0, RobotConfig.ELEVATOR_TALON_P, 0);
+		masterElevatorTalon.config_kI(0, RobotConfig.ELEVATOR_TALON_I, 0);
+		masterElevatorTalon.config_kD(0, RobotConfig.ELEVATOR_TALON_D, 0);
+		masterElevatorTalon.config_kF(0, RobotConfig.ELEVATOR_TALON_F, 0);
+
+		// ELEVATOR NODE SETUP
+		HIDAxisValueNode elevatorAxis = new HIDAxisValueNode(network, logger, "Elvator Axis", secondaryJoystick,
+				XBoxControllerLayout.Axis.LEFT_JOYSTICK_Y);
+
+		ElevatorNodeGroup elevatorNodeGroup = new ElevatorNodeGroup(network, logger, "Elevator Node Group",
+				elevatorAxis, RobotConfig.MAX_ELEVATOR_HEIGHT);
+
+		MeasurementToEncoderValueNode encoderElevatorNodeGroup = new MeasurementToEncoderValueNode(network, logger,
+				"Elevator Measurement Node Group", elevatorNodeGroup.getMaxHeightValueNode(),
+				RobotConfig.ELEVATOR_CONTROL_SHAFT_DIAMETER, RobotConfig.POSITION_PULSES_PER_ROTATION);
+
+		ConstantValueNode<ControlMode> elevatorControlMode = new ConstantValueNode<ControlMode>(network, logger,
+				"Elevator Control Mode", ControlMode.Position);
+
+		new TalonSRXNode(network, logger, "Elevator Talon", true, elevatorControlMode,
+				encoderElevatorNodeGroup.getEncoderPulsesValueNode(), masterElevatorTalon);
+
+		// INTAKE CLAMP SETUP
+		DoubleSolenoid intakeClampShiftingSolenoid = new DoubleSolenoid(RobotConfig.SOLENOID_MODULE_NUMBER,
+				RobotConfig.INTAKE_SOLENOID_FORWARD_CHANNEL, RobotConfig.INTAKE_SOLENOID_REVERSE_CHANNEL);
+
+		ShiftingNodeGroup intakeClampNodeGroup = new ShiftingNodeGroup(network, logger, "Intake Clamp Node Group",
+				secondaryJoystick, XBoxControllerLayout.Buttons.RB_BUTTON, XBoxControllerLayout.Buttons.LB_BUTTON,
+				intakeClampShiftingSolenoid, Value.kReverse);
+
+		new DoubleSolenoidNode(network, logger, "Intake Clamp Solenoid", true,
+				intakeClampNodeGroup.getSolenoidController(), intakeClampShiftingSolenoid);
+
+		// INTAKE TALON SETUP
+		TalonSRX intakeMasterTalon = new TalonSRX(RobotConfig.RIGHT_INTAKE_TALON_DEVICE_NUMBER);
+		TalonSRX intakeSlaveTalon = new TalonSRX(RobotConfig.LEFT_INTAKE_TALON_DEVICE_NUMBER);
+
+		intakeSlaveTalon.setInverted(true);
+
+		intakeSlaveTalon.set(ControlMode.Follower, RobotConfig.RIGHT_INTAKE_TALON_DEVICE_NUMBER);
+
+		// INTAKE MOTOR NODE
+		HIDAxisValueNode cubeIntakeAxis = new HIDAxisValueNode(network, logger, "Cube Intake Axis", secondaryJoystick,
+				XBoxControllerLayout.Axis.RIGHT_JOYSTICK_Y);
+
+		ConstantValueNode<ControlMode> intakeControlMode = new ConstantValueNode<ControlMode>(network, logger,
+				"Intake Control Mode", ControlMode.PercentOutput);
+
+		new TalonSRXNode(network, logger, "Intake Talon", true, intakeControlMode, cubeIntakeAxis, intakeMasterTalon);
 
 		network.start();
+
+		// IN PROGRESS AUTO CODE
+		// TODO Add auto code.
+		// RobotStateValueNode robotState = new RobotStateValueNode(network, logger,
+		// "Robot State", this);
+		// ValueNode<Double> autonLeftDrivetrainValueNode = null;
+		// ValueNode<Double> autonRightDrivetrainValueNode = null;
+		//
+		// ValueNode<Double> defaultRobotSpeed = new ConstantValueNode<Double>(network,
+		// logger, "Default Robot Speed", 0d);
+		//
+		// MultiplexerValueNode<Double, RobotState> leftRobotSpeed =
+		// Robot.<Double>generateAutonMultiplexerValueNode(
+		// network, logger, "Left Drivetrain Multiplexer", robotState,
+		// autonLeftDrivetrainValueNode,
+		// encoderSpeed.getLeftEncoderValue(), defaultRobotSpeed);
+		//
+		// MultiplexerValueNode<Double, RobotState> rightRobotSpeed =
+		// Robot.<Double>generateAutonMultiplexerValueNode(
+		// network, logger, "Right Drivetrain Multiplexer", robotState,
+		// autonRightDrivetrainValueNode,
+		// encoderSpeed.getRightEncoderValue(), defaultRobotSpeed);
+		//
+		// AutonomousAction[] autoPlanActions = { new MoveForwardAction(14, 3), new
+		// DeliverCubeAction(3) };
+		//
+		// ConstantValueNode<AutonomousAction[]> autoPlan = new
+		// ConstantValueNode<AutonomousAction[]>(network, logger,
+		// "Auto Plan", autoPlanActions);
+		//
+		// TalonSRXEncoderPositionValueNode rightEncoderPosition = new
+		// TalonSRXEncoderPositionValueNode(network, logger,
+		// "Right Talon Position", masterRight);
+		// TalonSRXEncoderPositionValueNode leftEncoderPosition = new
+		// TalonSRXEncoderPositionValueNode(network, logger,
+		// "Left Talon Position", masterLeft);
+		//
+		// EncoderToMeasurementNodeGroup rightEncoderMeasurementNodeGroup = new
+		// EncoderToMeasurementNodeGroup(network,
+		// logger, "Right Encoder Measurement", rightEncoderPosition,
+		// distancePulsesPerRotation, wheelDiameter);
+		// EncoderToMeasurementNodeGroup leftEncoderMeasurementNodeGroup = new
+		// EncoderToMeasurementNodeGroup(network,
+		// logger, "Left Encoder Measurement", leftEncoderPosition,
+		// distancePulsesPerRotation, wheelDiameter);
+		//
+		// AutoPlanFollower currentActionNode = new AutoPlanFollower(network, logger,
+		// "Auto Plan Follower", autoPlan, new TimerValueNode(network, logger, "Auto
+		// Plan Timer"), );
+		//
+		// TalonSRXControlModeValueNode controlMode = new
+		// TalonSRXControlModeValueNode(network, logger,
+		// "Drive Train Control Mode", robotState, currentActionNode);
 	}
 
 	public static <T> MultiplexerValueNode<T, RobotState> generateAutonMultiplexerValueNode(Network network,
---
+++ src/main/java/frc/team5940/codebase2018/robot/RobotConfig.java
@@ -0,0 +1,79 @@
+package frc.team5940.codebase2018.robot;
+
+public interface RobotConfig {
+
+	// TODO
+	public static final int MASTER_ELEVATOR_TALON_PORT = 5;
+
+	// TODO
+	public static final int SLAVE_ELEVATOR_TALON_PORT = 6;
+
+	// TODO
+	public static final double MAX_ELEVATOR_HEIGHT = 7.75;
+
+	// TODO
+	public static final double ELEVATOR_CONTROL_SHAFT_DIAMETER = 7.75;
+
+	// TODO
+	public static final double VELOCITY_PULSES_PER_ROTATION = 409.6f;
+
+	// TODO
+	public static final double POSITION_PULSES_PER_ROTATION = 4096f;
+
+	// TODO
+	public static final double ELEVATOR_TALON_P = 0;
+
+	// TODO
+	public static final double ELEVATOR_TALON_I = 0;
+
+	// TODO
+	public static final double ELEVATOR_TALON_D = 0;
+
+	// TODO
+	public static final double ELEVATOR_TALON_F = 0;
+
+	// TODO
+	public static final int SOLENOID_MODULE_NUMBER = 9;
+
+	// TODO
+	public static final int DRIVETRAIN_SOLENOID_SHIFTING_FORWARD_CHANNEL = 6;
+
+	// TODO
+	public static final int DRIVETRAIN_SOLENOID_SHIFTING_REVERSE_CHANNEL = 7;
+
+	// TODO
+	public static final int INTAKE_SOLENOID_FORWARD_CHANNEL = 8;
+
+	// TODO
+	public static final int INTAKE_SOLENOID_REVERSE_CHANNEL = 9;
+
+	// TODO
+	public static final int LEFT_INTAKE_TALON_DEVICE_NUMBER = 5;
+
+	// TODO
+	public static final int RIGHT_INTAKE_TALON_DEVICE_NUMBER = 6;
+
+	// TODO
+	public static final float CIABATTA_LOW_GEAR_POSITION_P = 0.5f;
+
+	// TODO
+	public static final float CIABATTA_LOW_GEAR_POSITION_I = 0f;
+
+	// TODO
+	public static final float CIABATTA_LOW_GEAR_POSITION_D = 20f;
+
+	// TODO
+	public static final float CIABATTA_LOW_GEAR_POSITION_F = 0f;
+
+	// TODO
+	public static final float CIABATTA_LOW_GEAR_VELOCITY_P = 0.9f;
+
+	// TODO
+	public static final float CIABATTA_LOW_GEAR_VELOCITY_I = 0.0025f;
+
+	// TODO
+	public static final float CIABATTA_LOW_GEAR_VELOCITY_D = 40f;
+
+	// TODO
+	public static final float CIABATTA_LOW_GEAR_VELOCITY_F = 0.15f;
+}
