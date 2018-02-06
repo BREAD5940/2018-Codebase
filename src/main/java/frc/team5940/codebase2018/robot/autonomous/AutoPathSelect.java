72d6cf72e53aa0e2e1c45a0b3da0960fba677741
Julia Reid <jdereid@gmail.com>
2018-02-05 17:06:16 -0800

    stufff lolz

--- src/main/java/frc/team5940/codebase2018/robot/autonomous/AutoPathSelect.java
+++ src/main/java/frc/team5940/codebase2018/robot/autonomous/AutoPathSelect.java
@@ -1,23 +1,27 @@
 package frc.team5940.codebase2018.robot.autonomous;
 
+import java.util.ArrayList;
+
 import org.team5940.pantry.logging.loggers.Logger;
+import org.team5940.pantry.logging.messages.events.WarningEventMessage;
 import org.team5940.pantry.processing_network.Network;
 import org.team5940.pantry.processing_network.ValueNode;
 
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import frc.team5940.codebase2018.robot.autonomous.AutoPathSelect.AutoPaths;
+import frc.team5940.codebase2018.robot.autonomous.AutoPathSelect.AutoPath;
 import frc.team5940.codebase2018.robot.autonomous.actions.AutoAction;
 import frc.team5940.codebase2018.robot.autonomous.actions.DriveAutoAction;
 import frc.team5940.codebase2018.robot.autonomous.actions.ElevatorDependentAutoAction;
 import frc.team5940.codebase2018.robot.autonomous.actions.OuttakeCubeAutoAction;
 import frc.team5940.codebase2018.robot.autonomous.actions.TurnAutoAction;
 
-public class AutoPathSelect extends ValueNode<Enum<? extends AutoPaths>> {
+public class AutoPathSelect extends ValueNode<Enum<? extends AutoPath>> {
 
 	SendableChooser<RobotLocation> robotLoc;
+	RobotLocation prevRobotLoc = null;
 	
-	public AutoPathSelect(Network network, Logger logger, ValueNode<Enum<? extends AutoPaths>>[] sourcesArray, String fmsReturn)
+	public AutoPathSelect(Network network, Logger logger, ValueNode<Enum<? extends AutoPath>>[] sourcesArray, String fmsReturn)
 			throws IllegalArgumentException, IllegalStateException {
 		super(network, logger, sourcesArray);
 		
@@ -34,7 +38,7 @@ public class AutoPathSelect extends ValueNode<Enum<? extends AutoPaths>> {
 		LEFT, CENTER, RIGHT; 
 	}
 	
-	public enum AutoPaths {
+	public enum AutoPath {
 		AUTO_LINE("Left AutoLine", RobotLocation.LEFT, new DriveAutoAction(11)), 
 		LEFT_PLACE_SWITCH("Place Switch From Left", RobotLocation.LEFT,new DriveAutoAction(14), new TurnAutoAction(90), new DriveAutoAction(1), new OuttakeCubeAutoAction(ElevatorDependentAutoAction.ElevatorHeight.SWITCH)), 
 		LEFT_PLACE_SCALE("Place Scale From Left",RobotLocation.LEFT, new DriveAutoAction(27), new TurnAutoAction(90), new DriveAutoAction(1), new OuttakeCubeAutoAction(ElevatorDependentAutoAction.ElevatorHeight.SCALE)),
@@ -42,12 +46,23 @@ public class AutoPathSelect extends ValueNode<Enum<? extends AutoPaths>> {
 		CENTER_PLACE_SWITCH_RIGHT("Place Right Switch From Center", RobotLocation.CENTER, new DriveAutoAction(4),/*right*/ new TurnAutoAction(90), new DriveAutoAction(4.5),/*left*/ new TurnAutoAction(6), new OuttakeCubeAutoAction(ElevatorDependentAutoAction.ElevatorHeight.SWITCH));
 		
 		AutoAction[] actions;
-		AutoPaths( String key, RobotLocation roboloc, AutoAction... actions) {
+		String key; 
+		RobotLocation roboLoc; 
+		AutoPath( String key, RobotLocation roboLoc, AutoAction... actions) {
 			this.actions = actions; 
-			
+			this.key = key; 
+			this.roboLoc = roboLoc; 
 			
 		}	
 		
+		public String getKey() {
+			return key;
+		
+		}
+		
+		public RobotLocation getRequiredRobotLocation() {
+			return roboLoc;
+		}
 		public AutoAction[] getActions() {
 			return actions;
 		}
@@ -57,11 +72,36 @@ public class AutoPathSelect extends ValueNode<Enum<? extends AutoPaths>> {
 	@Override
 	public boolean requiresUpdate() {
 		// TODO Auto-generated method stub
-		return super.requiresUpdate();
+		return true;
+		//TODO use michaels thing once he commits to githubsbssbsbusb
+		
 	}
 
 	@Override
-	protected Enum<? extends AutoPaths> updateValue() {
+	protected Enum<? extends AutoPath> updateValue() {
+		
+		if(prevRobotLoc != this.robotLoc.getSelected()) {
+			for(AutoPath path : AutoPath.values()) {
+				if(path.getRequiredRobotLocation() == this.robotLoc.getSelected()) {
+					SmartDashboard.putString(path.getKey(), "0"); 
+				}else {
+					SmartDashboard.putString(path.getKey(), "Disabled"); 
+				}
+			}
+		}
+		
+		ArrayList<AutoPath> autoPaths = new ArrayList<AutoPath>(); 
+		
+		for(AutoPath path : AutoPath.values()) {
+			try {
+				if(path.getRequiredRobotLocation() == this.robotLoc.getSelected() && Integer.parseInt(SmartDashboard.getString(path.getKey(), "0")) != 0) {
+					autoPaths.add(path); 
+				}
+			}catch (NumberFormatException e) {
+				this.logger.log(new WarningEventMessage(this, e));
+			}
+		}
+
 		
 		if(robotLoc.getSelected() == RobotLocation.CENTER) {
 			
@@ -71,6 +111,7 @@ public class AutoPathSelect extends ValueNode<Enum<? extends AutoPaths>> {
 			
 		}
 
+		prevRobotLoc = this.robotLoc.getSelected();
 		// TODO Auto-generated method stub
 		return null;
 	}
