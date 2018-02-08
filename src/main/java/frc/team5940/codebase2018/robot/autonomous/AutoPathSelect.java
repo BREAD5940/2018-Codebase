6297a37018797f7a9e3081900e517108699296bf
Julia Reid <jdereid@gmail.com>
2018-02-07 16:59:07 -0800

    fixed merge errorz

--- src/main/java/frc/team5940/codebase2018/robot/autonomous/AutoPathSelect.java
+++ src/main/java/frc/team5940/codebase2018/robot/autonomous/AutoPathSelect.java
@@ -42,25 +42,18 @@ public class AutoPathSelect extends ValueNode<Enum<? extends AutoPath>> {
 	}
 	
 	public enum AutoPath {
-<<<<<<< HEAD
+
 		AUTO_LINE("Left AutoLine", "XXX", RobotLocation.LEFT, new DriveAutoAction(11)), 
-		LEFT_PLACE_SWITCH("Place Switch From Left","LXX", RobotLocation.LEFT,new DriveAutoAction(14), new TurnAutoAction(90), new DriveAutoAction(1), new OuttakeCubeAutoAction(ElevatorDependentAutoAction.ElevatorHeight.SWITCH)), 
-		LEFT_PLACE_SCALE("Place Scale From Left","XLX", RobotLocation.LEFT, new DriveAutoAction(27), new TurnAutoAction(90), new DriveAutoAction(1), new OuttakeCubeAutoAction(ElevatorDependentAutoAction.ElevatorHeight.SCALE)),
-		CENTER_PLACE_SWITCH_LEFT("Place Left Switch From Center","LXX", RobotLocation.CENTER, new DriveAutoAction(4),/*left*/ new TurnAutoAction(90), new DriveAutoAction(4.5),/*right*/ new TurnAutoAction(6), new OuttakeCubeAutoAction(ElevatorDependentAutoAction.ElevatorHeight.SWITCH)), 
-		CENTER_PLACE_SWITCH_RIGHT("Place Right Switch From Center","RXX", RobotLocation.CENTER, new DriveAutoAction(4),/*right*/ new TurnAutoAction(90), new DriveAutoAction(4.5),/*left*/ new TurnAutoAction(6), new OuttakeCubeAutoAction(ElevatorDependentAutoAction.ElevatorHeight.SWITCH));
-		//TODO add right side auto pathz 
-=======
-		AUTO_LINE("Left AutoLine", RobotLocation.LEFT, new DriveAutoAction(11)), 
-		LEFT_PLACE_SWITCH("Place Switch From Left", RobotLocation.LEFT,new DriveAutoAction(14), new TurnAutoAction(90), new DriveAutoAction(1), new OuttakeCubeAutoAction()), 
-		LEFT_PLACE_SCALE("Place Scale From Left",RobotLocation.LEFT, new DriveAutoAction(27), new TurnAutoAction(90), new DriveAutoAction(1), new OuttakeCubeAutoAction()),
-		RIGHT_PLACE_SWITCH("Place Switch From Right", RobotLocation.RIGHT,new DriveAutoAction(14), new TurnAutoAction(-90), new DriveAutoAction(1), new OuttakeCubeAutoAction()), 
-		RIGHT_PLACE_SCALE("Place Scale From Right",RobotLocation.RIGHT, new DriveAutoAction(27), new TurnAutoAction(-90), new DriveAutoAction(1), new OuttakeCubeAutoAction()),
-		CENTER_PLACE_SWITCH_LEFT("Place Left Switch From Center", RobotLocation.CENTER, new DriveAutoAction(4),/*left*/ new TurnAutoAction(-90), new DriveAutoAction(4.5),/*right*/ new TurnAutoAction(6), new OuttakeCubeAutoAction()), 
-		CENTER_PLACE_SWITCH_RIGHT("Place Right Switch From Center", RobotLocation.CENTER, new DriveAutoAction(4),/*right*/ new TurnAutoAction(90), new DriveAutoAction(4.5),/*left*/ new TurnAutoAction(6), new OuttakeCubeAutoAction());
+		LEFT_PLACE_SWITCH("Place Switch From Left","LXX", RobotLocation.LEFT,new DriveAutoAction(14), new TurnAutoAction(90), new DriveAutoAction(1), new OuttakeCubeAutoAction()), 
+		LEFT_PLACE_SCALE("Place Scale From Left","XLX", RobotLocation.LEFT, new DriveAutoAction(27), new TurnAutoAction(90), new DriveAutoAction(1), new OuttakeCubeAutoAction()),
+		RIGHT_PLACE_SWITCH("Place Switch From Right","RXX", RobotLocation.RIGHT,new DriveAutoAction(14), new TurnAutoAction(-90), new DriveAutoAction(1), new OuttakeCubeAutoAction()), 
+		RIGHT_PLACE_SCALE("Place Scale From Right","XLX",RobotLocation.RIGHT, new DriveAutoAction(27), new TurnAutoAction(-90), new DriveAutoAction(1), new OuttakeCubeAutoAction()),
+		CENTER_PLACE_SWITCH_LEFT("Place Left Switch From Center","LXX", RobotLocation.CENTER, new DriveAutoAction(4),/*left*/ new TurnAutoAction(90), new DriveAutoAction(4.5),/*right*/ new TurnAutoAction(6), new OuttakeCubeAutoAction()), 
+		CENTER_PLACE_SWITCH_RIGHT("Place Right Switch From Center","RXX", RobotLocation.CENTER, new DriveAutoAction(4),/*right*/ new TurnAutoAction(90), new DriveAutoAction(4.5),/*left*/ new TurnAutoAction(6), new OuttakeCubeAutoAction());
 		//#TODO: Fix elevator Args
 		
 		
->>>>>>> b3ce07e2e13824f51a55db7a2ab30c60e65f0bd6
+
 		AutoAction[] actions;
 		String key; 
 		RobotLocation roboLoc; 
