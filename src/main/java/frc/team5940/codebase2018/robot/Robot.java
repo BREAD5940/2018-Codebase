e2d0f41a23e1eca8412cfc05681070eeb9f9eee0
bertturtle <mbentley124@gmail.com>
2018-02-17 15:09:32 -0800

    Update Robot.java

--- src/main/java/frc/team5940/codebase2018/robot/Robot.java
+++ src/main/java/frc/team5940/codebase2018/robot/Robot.java
@@ -1,30 +1,10 @@
 package frc.team5940.codebase2018.robot;
 
-import org.team5940.pantry.logging.loggers.Logger;
-import org.team5940.pantry.logging.loggers.PrintStreamLogger;
-import org.team5940.pantry.processing_network.Network;
-import org.team5940.pantry.processing_network.wpilib.output.ObjectSmartDashboardNode;
-import org.team5940.pantry.processing_network.wpilib.systems.controller_layouts.XBoxControllerLayout;
-
 import edu.wpi.first.wpilibj.IterativeRobot;
-import edu.wpi.first.wpilibj.Joystick;
 
 public class Robot extends IterativeRobot {
 	@Override
 	public void robotInit() {
-		Logger logger = new PrintStreamLogger(System.out);
-		Network network = new Network(20000, logger);
-		Joystick joystick = new Joystick(0);
-		RampSetupNodeGroup rampNodeGroup = new RampSetupNodeGroup(network, logger, "Ramp", joystick, joystick,
-				XBoxControllerLayout.Buttons.LB_BUTTON, XBoxControllerLayout.Buttons.RB_BUTTON);
-
-		new ObjectSmartDashboardNode(network, logger, "Ramp Piston State", true, "Ramp Piston State",
-				rampNodeGroup.getRampPistonValue());
-
-		new ObjectSmartDashboardNode(network, logger, "Wheel Piston State", true, "Wheel Piston State",
-				rampNodeGroup.getWheelPistonValue());
-
-		network.start();
 	}
 
 	@Override
