2e539b3de5657846ce7037d5e9ce8fa57dd01cf5
BertTurtle <mbentley124@gmail.com>
2018-02-17 11:25:50 -0800

    Updated RobotConfig to have more constants

--- src/main/java/frc/team5940/codebase2018/robot/RobotConfig.java
+++ src/main/java/frc/team5940/codebase2018/robot/RobotConfig.java
@@ -4,6 +4,8 @@ import org.team5940.pantry.processing_network.wpilib.systems.controller_layouts.
 
 public interface RobotConfig {
 
+	public static final boolean TESTING_MODE = true;
+
 	// JOYSTICK CONFIGURATION
 	public static final int INTAKE_CLAMP_BUTTON = XBoxControllerLayout.Buttons.RB_BUTTON;
 
@@ -18,6 +20,10 @@ public interface RobotConfig {
 	public static final int DRIVETRAIN_YAW_AXIS = XBoxControllerLayout.Axis.RIGHT_JOYSTICK_X;
 
 	public static final int DRIVETRAIN_FORWARD_AXIS = XBoxControllerLayout.Axis.LEFT_JOYSTICK_Y;
+
+	public static final double DRIVETRAIN_YAW_AXIS_DEADZONE = 0.05;
+
+	public static final double DRIVETRAIN_FORWARD_AXIS_DEADZONE = 0.05;
 
 	public static final int SHIFT_UP_BUTTON = XBoxControllerLayout.Buttons.RB_BUTTON;
 
@@ -35,16 +41,72 @@ public interface RobotConfig {
 	// TODO
 	public static final int RIGHT_RAMP_RAISE_BUTTON = XBoxControllerLayout.Buttons.Y_BUTTON;
 
+	// AXIS INVERSION
+	boolean DRIVETRAIN_FORWARD_AXIS_INVERTED = true;
+
+	boolean DRIVETRAIN_YAW_AXIS_INVERTED = false;
+
+	boolean CUBE_INTAKE_YAW_AXIS_INVERTED = false;
+
+	boolean CUBE_INTAKE_FORWARD_AXIS_INVERTED = true;
+
+	// TODO
+	boolean ELEVATOR_AXIS_INVERTED = true;
+
+	// I did something odd below so people don't accidently change the wrong value.
+	// This should work.
 	// REQUIRES UPDATE
-	public static final boolean INTAKE_TALONS_REQUIRE_UPDATE = true;
+	public static final boolean INTAKE_TALONS_REQUIRE_UPDATE = TESTING_MODE ? !TESTING_MODE : true;
 
-	public static final boolean INTAKE_PNEUMATICS_REQUIRE_UPDATE = true;
+	public static final boolean INTAKE_PNEUMATICS_REQUIRE_UPDATE = TESTING_MODE ? !TESTING_MODE : true;
 
-	public static final boolean ELEVATOR_TALON_REQUIRE_UPDATE = true;
+	public static final boolean ELEVATOR_TALON_REQUIRE_UPDATE = TESTING_MODE ? !TESTING_MODE : true;
 
-	public static final boolean DRIVETRAIN_TALONS_REQUIRE_UPDATE = true;
+	public static final boolean DRIVETRAIN_TALONS_REQUIRE_UPDATE = TESTING_MODE ? !TESTING_MODE : true;
 
-	public static final boolean DRIVETRAIN_SHIFTING_REQUIRE_UPDATE = true;
+	public static final boolean DRIVETRAIN_SHIFTING_REQUIRE_UPDATE = TESTING_MODE ? !TESTING_MODE : true;
+
+	// I did something odd below so people don't accidently change the wrong value.
+	// This should work.
+	// REQUIRES UPDATE
+	public static final boolean LEFT_INTAKE_TALON_SMARTDASHBOARD_REQUIRE_UPDATE = TESTING_MODE ? TESTING_MODE : false;
+
+	public static final boolean RIGHT_INTAKE_TALON_SMARTDASHBOARD_REQUIRE_UPDATE = TESTING_MODE ? TESTING_MODE : false;
+
+	public static final boolean INTAKE_PNEUMATICS_SMARTDASHBOARD_REQUIRE_UPDATE = TESTING_MODE ? TESTING_MODE : false;
+
+	public static final boolean ELEVATOR_TALON_SMARTDASHBOARD_REQUIRE_UPDATE = TESTING_MODE ? TESTING_MODE : false;
+
+	public static final boolean ELEVATOR_PARAMETER_SLOT_SMARTDASHBOARD_REQUIRE_UPDATE = TESTING_MODE ? TESTING_MODE
+			: false;
+
+	public static final boolean LEFT_DRIVETRAIN_TALONS_SMARTDASHBOARD_REQUIRE_UPDATE = TESTING_MODE ? TESTING_MODE
+			: false;
+
+	public static final boolean RIGHT_DRIVETRAIN_TALONS_SMARTDASHBOARD_REQUIRE_UPDATE = TESTING_MODE ? TESTING_MODE
+			: false;
+
+	public static final boolean DRIVETRAIN_SHIFTING_SMARTDASHBOARD_REQUIRE_UPDATE = TESTING_MODE ? TESTING_MODE : false;
+
+	public static final boolean DRIVETRAIN_PARAMETER_SLOT_SMARTDASHBOARD_REQUIRE_UPDATE = TESTING_MODE ? TESTING_MODE
+			: false;
+
+	public static final boolean DRIVETRAIN_CONTROLMODE_SMARTDASHBOARD_REQUIRE_UPDATE = TESTING_MODE ? TESTING_MODE
+			: false;
+
+	public static final boolean RIGHT_ENCODER_POSITION_SMARTDASHBOARD_REQUIRE_UPDATE = TESTING_MODE ? TESTING_MODE
+			: false;;
+
+	public static final boolean ELEVATOR_CURRENT_HEIGHT_SMARTDASHBOARD_REQUIRE_UPDATE = TESTING_MODE ? TESTING_MODE
+			: false;;
+
+	public static final boolean CUBE_SENSOR_SMARTDASHBOARD_REQUIRE_UPDATE = TESTING_MODE ? TESTING_MODE : false;;
+
+	public static final boolean AUTO_PATH_SMARTDASHBOARD_REQUIRE_UPDATE = TESTING_MODE ? TESTING_MODE : false;;
+
+	public static final boolean CURRENT_ACTION_SMARTDASHBOARD_REQUIRE_UPDATE = TESTING_MODE ? TESTING_MODE : false;;
+
+	public static final boolean ROBOT_ANGLE_SMARTDASHBOARD_REQUIRE_UPDATE = TESTING_MODE ? TESTING_MODE : false;;
 
 	// TALON PORTS
 	public static final int SLAVE_LEFT_TALON_PORT = 1;
@@ -68,17 +130,13 @@ public interface RobotConfig {
 	public static final int RIGHT_INTAKE_TALON_PORT = 8;
 
 	// ELEVATOR
-	// TODO
-	public static final double MAX_ELEVATOR_HEIGHT = 7.75;
+	public static final double MAX_ELEVATOR_HEIGHT = 84.5 / 12;
 
-	// TODO
-	public static final double ELEVATOR_CONTROL_SHAFT_DIAMETER = 7.75;
+	public static final double ELEVATOR_CONTROL_SHAFT_DIAMETER = 1.273f / 12;
 
-	// TODO
-	public static final int RAISE_ELEVATOR_SLOT_ID = 1;
+	public static final int RAISE_ELEVATOR_SLOT_ID = 2;
 
-	// TODO
-	public static final int LOWER_ELEVATOR_SLOT_ID = 0;
+	public static final int LOWER_ELEVATOR_SLOT_ID = 1;
 
 	// ELEVATOR PID
 	// TODO
@@ -120,7 +178,7 @@ public interface RobotConfig {
 	public static final int DRIVETRAIN_SOLENOID_SHIFTING_REVERSE_CHANNEL = 7;
 
 	// TODO
-	public static final float WHEEL_DIAMETER = 3.5f / 12;
+	public static final float WHEEL_DIAMETER = 6f / 12;
 
 	// TODO
 	public static final int LOW_GEAR_VELOCITY_SLOT_ID = 1;
@@ -169,16 +227,14 @@ public interface RobotConfig {
 	public static final float NEW_ROBOT_LOW_GEAR_VELOCITY_F = 0.15f;
 
 	// TODO
-	public static final float NEW_ROBOT_LOW_GEAR_MAX_VELOCITY = 0.15f;
+	public static final float NEW_ROBOT_LOW_GEAR_MAX_VELOCITY = 7f;
 
 	// TODO
-	public static final float NEW_ROBOT_HIGH_GEAR_MAX_VELOCITY = 0.15f;
+	public static final float NEW_ROBOT_HIGH_GEAR_MAX_VELOCITY = 15f;
 
 	// GENERAL
-	// TODO
 	public static final double VELOCITY_PULSES_PER_ROTATION = 409.6f;
 
-	// TODO
 	public static final double POSITION_PULSES_PER_ROTATION = 4096f;
 
 	// TODO
@@ -208,4 +264,7 @@ public interface RobotConfig {
 
 	// TODO
 	public static final float CIABATTA_LOW_GEAR_VELOCITY_F = 0.15f;
+
+	// TODO
+	public static final int INTAKE_SENSOR_DIGITAL_INPUT_PORT = 0;
 }
