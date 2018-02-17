7ff147b27f4b34b9acc93a661b58285d7f4d5304
paul <paul@programmingCompooter>
2018-02-17 14:49:32 -0800

    Created NodeGroup for ramp nodes and added it to robot.java

--- src/main/java/frc/team5940/codebase2018/robot/Robot.java
+++ src/main/java/frc/team5940/codebase2018/robot/Robot.java
@@ -1,33 +1,61 @@
 package frc.team5940.codebase2018.robot;
 
+import org.team5940.pantry.logging.loggers.Logger;
+import org.team5940.pantry.logging.loggers.PrintStreamLogger;
+import org.team5940.pantry.processing_network.Network;
+import org.team5940.pantry.processing_network.wpilib.output.ObjectSmartDashboardNode;
+import org.team5940.pantry.processing_network.wpilib.systems.controller_layouts.XBoxControllerLayout;
+
 import edu.wpi.first.wpilibj.IterativeRobot;
+import edu.wpi.first.wpilibj.Joystick;
 
 public class Robot extends IterativeRobot {
-    @Override
-    public void robotInit() { }
+	@Override
+	public void robotInit() {
+		Logger logger = new PrintStreamLogger(System.out);
+		Network network = new Network(20000, logger);
+		Joystick joystick = new Joystick(0);
+		RampSetupNodeGroup rampNodeGroup = new RampSetupNodeGroup(network, logger, "Ramp", joystick, joystick,
+				XBoxControllerLayout.Buttons.LB_BUTTON, XBoxControllerLayout.Buttons.RB_BUTTON);
 
-    @Override
-    public void disabledInit() { }
+		new ObjectSmartDashboardNode(network, logger, "Ramp Piston State", true, "Ramp Piston State",
+				rampNodeGroup.getRampPistonValue());
 
-    @Override
-    public void autonomousInit() { }
+		new ObjectSmartDashboardNode(network, logger, "Wheel Piston State", true, "Wheel Piston State",
+				rampNodeGroup.getWheelPistonValue());
 
-    @Override
-    public void teleopInit() { }
+		network.start();
+	}
 
-    @Override
-    public void testInit() { }
+	@Override
+	public void disabledInit() {
+	}
 
+	@Override
+	public void autonomousInit() {
+	}
 
-    @Override
-    public void disabledPeriodic() { }
-    
-    @Override
-    public void autonomousPeriodic() { }
+	@Override
+	public void teleopInit() {
+	}
 
-    @Override
-    public void teleopPeriodic() { }
+	@Override
+	public void testInit() {
+	}
 
-    @Override
-    public void testPeriodic() { }
+	@Override
+	public void disabledPeriodic() {
+	}
+
+	@Override
+	public void autonomousPeriodic() {
+	}
+
+	@Override
+	public void teleopPeriodic() {
+	}
+
+	@Override
+	public void testPeriodic() {
+	}
 }
