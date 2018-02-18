58179000164f91e391813fbeb5d72b81c9033659
BertTurtle <mbentley124@gmail.com>
2018-02-17 17:19:11 -0800

    Updated controls for new joystick

--- src/main/java/frc/team5940/codebase2018/robot/RobotConfig.java
+++ src/main/java/frc/team5940/codebase2018/robot/RobotConfig.java
@@ -1,21 +1,22 @@
 package frc.team5940.codebase2018.robot;
 
+import org.team5940.pantry.processing_network.wpilib.systems.controller_layouts.LogitechJoystickControllerLayout;
 import org.team5940.pantry.processing_network.wpilib.systems.controller_layouts.XBoxControllerLayout;
 
 public interface RobotConfig {
 
-	public static final boolean TESTING_MODE = true;
+	public static final boolean TESTING_MODE = false;
 
 	// JOYSTICK CONFIGURATION
-	public static final int INTAKE_CLAMP_BUTTON = XBoxControllerLayout.Buttons.RB_BUTTON;
+	public static final int INTAKE_CLAMP_BUTTON = LogitechJoystickControllerLayout.Buttons.BUTTON_3;
 
-	public static final int INTAKE_UNCLAMP_BUTTON = XBoxControllerLayout.Buttons.LB_BUTTON;
+	public static final int INTAKE_UNCLAMP_BUTTON = LogitechJoystickControllerLayout.Buttons.BUTTON_4;
 
-	public static final int INTAKE_CONTROL_FORWARD_AXIS = XBoxControllerLayout.Axis.LEFT_JOYSTICK_Y;
+	public static final int INTAKE_CONTROL_FORWARD_AXIS = LogitechJoystickControllerLayout.Axis.JOYSTICK_Y;
 
-	public static final int INTAKE_CONTROL_YAW_AXIS = XBoxControllerLayout.Axis.LEFT_JOYSTICK_X;
+	public static final int INTAKE_CONTROL_YAW_AXIS = LogitechJoystickControllerLayout.Axis.JOYSTICK_ROTATE;
 
-	public static final int ELEVATOR_CONTROL_AXIS = XBoxControllerLayout.Axis.RIGHT_JOYSTICK_Y;
+	public static final int ELEVATOR_CONTROL_AXIS = LogitechJoystickControllerLayout.Axis.SLIDER;
 
 	public static final int DRIVETRAIN_YAW_AXIS = XBoxControllerLayout.Axis.RIGHT_JOYSTICK_X;
 
@@ -50,7 +51,6 @@ public interface RobotConfig {
 
 	boolean CUBE_INTAKE_FORWARD_AXIS_INVERTED = true;
 
-	// TODO
 	boolean ELEVATOR_AXIS_INVERTED = true;
 
 	// I did something odd below so people don't accidently change the wrong value.
@@ -73,9 +73,9 @@ public interface RobotConfig {
 
 	public static final boolean RIGHT_INTAKE_TALON_SMARTDASHBOARD_REQUIRE_UPDATE = TESTING_MODE ? TESTING_MODE : false;
 
-	public static final boolean INTAKE_PNEUMATICS_SMARTDASHBOARD_REQUIRE_UPDATE = TESTING_MODE ? TESTING_MODE : false;
+	public static final boolean INTAKE_PNEUMATICS_SMARTDASHBOARD_REQUIRE_UPDATE = TESTING_MODE ? TESTING_MODE : true;
 
-	public static final boolean ELEVATOR_TALON_SMARTDASHBOARD_REQUIRE_UPDATE = TESTING_MODE ? TESTING_MODE : false;
+	public static final boolean ELEVATOR_TALON_SMARTDASHBOARD_REQUIRE_UPDATE = TESTING_MODE ? TESTING_MODE : true;
 
 	public static final boolean ELEVATOR_PARAMETER_SLOT_SMARTDASHBOARD_REQUIRE_UPDATE = TESTING_MODE ? TESTING_MODE
 			: false;
@@ -86,7 +86,7 @@ public interface RobotConfig {
 	public static final boolean RIGHT_DRIVETRAIN_TALONS_SMARTDASHBOARD_REQUIRE_UPDATE = TESTING_MODE ? TESTING_MODE
 			: false;
 
-	public static final boolean DRIVETRAIN_SHIFTING_SMARTDASHBOARD_REQUIRE_UPDATE = TESTING_MODE ? TESTING_MODE : false;
+	public static final boolean DRIVETRAIN_SHIFTING_SMARTDASHBOARD_REQUIRE_UPDATE = TESTING_MODE ? TESTING_MODE : true;
 
 	public static final boolean DRIVETRAIN_PARAMETER_SLOT_SMARTDASHBOARD_REQUIRE_UPDATE = TESTING_MODE ? TESTING_MODE
 			: false;
@@ -95,18 +95,18 @@ public interface RobotConfig {
 			: false;
 
 	public static final boolean RIGHT_ENCODER_POSITION_SMARTDASHBOARD_REQUIRE_UPDATE = TESTING_MODE ? TESTING_MODE
-			: false;;
+			: false;
 
 	public static final boolean ELEVATOR_CURRENT_HEIGHT_SMARTDASHBOARD_REQUIRE_UPDATE = TESTING_MODE ? TESTING_MODE
-			: false;;
+			: true;
 
-	public static final boolean CUBE_SENSOR_SMARTDASHBOARD_REQUIRE_UPDATE = TESTING_MODE ? TESTING_MODE : false;;
+	public static final boolean CUBE_SENSOR_SMARTDASHBOARD_REQUIRE_UPDATE = TESTING_MODE ? TESTING_MODE : true;
 
-	public static final boolean AUTO_PATH_SMARTDASHBOARD_REQUIRE_UPDATE = TESTING_MODE ? TESTING_MODE : false;;
+	public static final boolean AUTO_PATH_SMARTDASHBOARD_REQUIRE_UPDATE = TESTING_MODE ? TESTING_MODE : true;
 
-	public static final boolean CURRENT_ACTION_SMARTDASHBOARD_REQUIRE_UPDATE = TESTING_MODE ? TESTING_MODE : false;;
+	public static final boolean CURRENT_ACTION_SMARTDASHBOARD_REQUIRE_UPDATE = TESTING_MODE ? TESTING_MODE : false;
 
-	public static final boolean ROBOT_ANGLE_SMARTDASHBOARD_REQUIRE_UPDATE = TESTING_MODE ? TESTING_MODE : false;;
+	public static final boolean ROBOT_ANGLE_SMARTDASHBOARD_REQUIRE_UPDATE = TESTING_MODE ? TESTING_MODE : true;
 
 	// TALON PORTS
 	public static final int SLAVE_LEFT_TALON_PORT = 1;
@@ -121,13 +121,10 @@ public interface RobotConfig {
 	public static final int MASTER_ELEVATOR_TALON_PORT = 5;
 
 	// TODO
-	public static final int SLAVE_ELEVATOR_TALON_PORT = 6;
+	public static final int LEFT_INTAKE_TALON_PORT = 6;
 
 	// TODO
-	public static final int LEFT_INTAKE_TALON_PORT = 7;
-
-	// TODO
-	public static final int RIGHT_INTAKE_TALON_PORT = 8;
+	public static final int RIGHT_INTAKE_TALON_PORT = 7;
 
 	// ELEVATOR
 	public static final double MAX_ELEVATOR_HEIGHT = 84.5 / 12;
@@ -227,18 +224,17 @@ public interface RobotConfig {
 	public static final float NEW_ROBOT_LOW_GEAR_VELOCITY_F = 0.15f;
 
 	// TODO
-	public static final float NEW_ROBOT_LOW_GEAR_MAX_VELOCITY = 7f;
+	public static final float NEW_ROBOT_LOW_GEAR_MAX_VELOCITY = 6f;
 
 	// TODO
-	public static final float NEW_ROBOT_HIGH_GEAR_MAX_VELOCITY = 15f;
+	public static final float NEW_ROBOT_HIGH_GEAR_MAX_VELOCITY = 12f;
 
 	// GENERAL
 	public static final double VELOCITY_PULSES_PER_ROTATION = 409.6f;
 
 	public static final double POSITION_PULSES_PER_ROTATION = 4096f;
 
-	// TODO
-	public static final int SOLENOID_MODULE_NUMBER = 9;
+	public static final int SOLENOID_MODULE_NUMBER = 0;
 
 	// CIABATTA
 	// TODO
