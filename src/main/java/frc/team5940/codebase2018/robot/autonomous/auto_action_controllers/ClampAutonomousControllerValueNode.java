961bdb28ac86da0b979c1ab343ca069b25683e8d
BertTurtle <mbentley124@gmail.com>
2018-11-10 23:46:30 -0500

    Added files to have clamp control during auton

--- src/main/java/frc/team5940/codebase2018/robot/autonomous/auto_action_controllers/ClampAutonomousControllerValueNode.java
+++ src/main/java/frc/team5940/codebase2018/robot/autonomous/auto_action_controllers/ClampAutonomousControllerValueNode.java
@@ -1,5 +1,6 @@
 package frc.team5940.codebase2018.robot.autonomous.auto_action_controllers;
 
+import frc.team5940.codebase2018.robot.autonomous.auto_actions.ClampAutoAction;
 import org.team5940.pantry.logging.loggers.Logger;
 import org.team5940.pantry.processing_network.Network;
 import org.team5940.pantry.processing_network.ValueNode;
@@ -8,18 +9,25 @@ import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
 import frc.team5940.codebase2018.robot.autonomous.auto_actions.AutoAction;
 
-public class ClampAutonomousControllerValueNode extends ValueNode<DoubleSolenoid.Value>{
+public class ClampAutonomousControllerValueNode extends ValueNode<DoubleSolenoid.Value> {
 
-	public ClampAutonomousControllerValueNode(Network network, Logger logger, String label, ValueNode<? extends AutoAction> autoActionValueNode)
-			throws IllegalArgumentException, IllegalStateException {
+	ValueNode<? extends AutoAction> autoActionValueNode;
+	DoubleSolenoid.Value solenoidValue = Value.kReverse;
+
+	public ClampAutonomousControllerValueNode(Network network, Logger logger, String label, ValueNode<?
+					extends AutoAction> autoActionValueNode)
+					throws IllegalArgumentException, IllegalStateException {
 		super(network, logger, label, autoActionValueNode);
-		// TODO Auto-generated constructor stub
+
+		this.autoActionValueNode = autoActionValueNode;
 	}
 
 	@Override
 	protected Value updateValue() {
-		// TODO Auto-generated method stub
-		return null;
+		if (this.autoActionValueNode.getValue() instanceof ClampAutoAction) {
+			this.solenoidValue = ((ClampAutoAction) this.autoActionValueNode.getValue()).getSolenoidValue();
+		}
+		return this.solenoidValue;
 	}
 
 }
